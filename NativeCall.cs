3eb950264c848534b4ddf02aa11bcdba7c7f0b13
unknown <castaneai@plesvz.(none)>
2012-03-11 18:32:37 +0900

    Hello git!

---
+++ NativeCall.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WindowController
+{
+	[System.Security.SuppressUnmanagedCodeSecurity()]
+	internal sealed class NativeCall
+	{
+		internal static bool IsWindowInForeground(IntPtr hWnd)
+		{
+			return hWnd == GetForegroundWindow();
+		}
+
+		internal enum WindowShowStyle : uint
+		{
+			Hide = 0,
+			ShowNormal = 1,
+			ShowMinimized = 2,
+			ShowMaximized = 3,
+			Maximize = 3,
+			ShowNormalNoActivate = 4,
+			Show = 5,
+			Minimize = 6,
+			ShowMinNoActivate = 7,
+			ShowNoActivate = 8,
+			Restore = 9,
+			ShowDefault = 10,
+			ForceMinimized = 11
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct POINT
+		{
+			public int X;
+			public int Y;
+
+			public POINT(int x, int y)
+			{
+				this.X = x;
+				this.Y = y;
+			}
+
+			public static implicit operator System.Drawing.Point(POINT p)
+			{
+				return new System.Drawing.Point(p.X, p.Y);
+			}
+
+			public static implicit operator POINT(System.Drawing.Point p)
+			{
+				return new POINT(p.X, p.Y);
+			}
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal struct RECT
+		{
+			public int left, top, right, bottom;
+		}
+
+		[DllImport("user32.dll")]
+		internal static extern bool ShowWindow(IntPtr hWnd, WindowShowStyle nCmdShow);
+
+		[DllImport("user32.dll")]
+		internal static extern IntPtr GetForegroundWindow();
+
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		internal static extern bool SetForegroundWindow(IntPtr hWnd);
+
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		internal static extern bool IsIconic(IntPtr hWnd);
+
+		[DllImport("user32.dll")]
+		internal static extern bool GetClientRect(IntPtr hwnd, out RECT lpRect);
+
+		[DllImport("user32.dll")]
+		internal static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+
+		[DllImport("user32.dll")]
+		internal static extern bool ClientToScreen(IntPtr hwnd, out POINT lpPoint);
+	}
+}
---
+++ Window.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace WindowController
+{
+	/// <summary>
+	/// 操作対象のウィンドウを表すクラス
+	/// </summary>
+	public class Window
+	{
+		/// <summary>
+		/// 対象のウィンドウのハンドル
+		/// </summary>
+		protected IntPtr handle;
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="processName">対象とするウィンドウを持つプロセスの名前</param>
+		public Window(string processName)
+		{
+			foreach (Process process in Process.GetProcessesByName(processName))
+			{
+				IntPtr handle = process.MainWindowHandle;
+				if (handle == IntPtr.Zero)
+					continue;
+				this.handle = handle;
+			}
+			if (this.handle == IntPtr.Zero)
+			{
+				throw new Exception(
+					string.Format("{0}のウィンドウが見つかりません。", processName)
+				);
+			}
+		}
+
+		/// <summary>
+		/// ウィドウが最小化されている場合、元に戻す
+		/// </summary>
+		public void Restore()
+		{
+			if (NativeCall.IsIconic(this.handle))
+				NativeCall.ShowWindow(this.handle, NativeCall.WindowShowStyle.Restore);
+		}
+
+		/// <summary>
+		/// クライアント領域を画像としてキャプチャする
+		/// </summary>
+		/// <param name="rectangle">クライアント領域内のキャプチャしたい領域。指定しない場合はクライアント領域全体をキャプチャする。</param>
+		/// <returns>キャプチャした画像データ</returns>
+		public Bitmap CaptureImage(Rectangle rectangle = default(Rectangle))
+		{
+			// ウィンドウが最小化されていた場合、もとに戻す
+			// （ウィンドウが最小化されていると座標を取得できないから）
+			this.Restore();
+
+			// クライアント領域の左上のスクリーン座標を取得
+			NativeCall.POINT screenPoint = new NativeCall.POINT(0, 0);
+			NativeCall.ClientToScreen(this.handle, out screenPoint);
+
+			// キャプチャ領域を取得
+			NativeCall.RECT clientRect = new NativeCall.RECT();
+			NativeCall.GetClientRect(this.handle, out clientRect);
+			if (rectangle == default(Rectangle))
+			{
+				rectangle = new Rectangle(
+					clientRect.left,
+					clientRect.top,
+					clientRect.right - clientRect.left,
+					clientRect.bottom - clientRect.top
+				);
+			}
+			Point captureStartPoint = new Point(
+				screenPoint.X + rectangle.X,
+				screenPoint.Y + rectangle.Y
+			);
+			// キャプチャ実行
+			Bitmap bitmap = new Bitmap(rectangle.Width, rectangle.Height);
+			Graphics graphics = Graphics.FromImage(bitmap);
+			using (graphics)
+			{
+				graphics.CopyFromScreen(captureStartPoint, new Point(0, 0), rectangle.Size);
+			}
+			return bitmap;
+		}
+	}
+}
